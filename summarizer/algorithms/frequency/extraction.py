53eb188f211521565d1889b090b7ae00001ad363
shubham joshi <shubham.joshi1507@gmail.com>
2020-07-31 16:14:30 +0530

    Update extraction.py

--- summarizer/algorithms/frequency/extraction.py
+++ summarizer/algorithms/frequency/extraction.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 
 
 # def visible(element):
@@ -9,11 +9,4 @@ from bs4 import BeautifulSoup
 #
 
 def extract(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    soup_tag = list(filter(lambda p: len(list(p.children)) < 2, soup.find_all(['p', 'div'], class_=None, id=None)))
-    text = ' '.join(map(lambda p: p.text, soup_tag))
-    if text == '':
-        text = 'No Paragraphs Found!'
-    text = text.replace('\xa0', ' ')
-    return text
+    pass
