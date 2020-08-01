e33e19502f98bce47be2f520a6e3a7a08284fe8d
shubham joshi <shubham.joshi1507@gmail.com>
2020-08-01 08:58:09 +0530

    Update settings.py

--- AutomaticTextSummarizer/settings.py
+++ AutomaticTextSummarizer/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '-1')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['3.6.40.17']
 
 # Application definition
 
