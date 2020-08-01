bc5b8d174f8c3365acdce1716fd87f8d3a5ccd3f
shubham joshi <shubham.joshi1507@gmail.com>
2020-08-01 19:48:33 +0530

    Update settings.py

--- AutomaticTextSummarizer/settings.py
+++ AutomaticTextSummarizer/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '-1')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.6.40.17','automatictext.herokuapp.com']
+ALLOWED_HOSTS = ['3.6.40.17','automatictext.herokuapp.com','13.232.4.96:8000']
 
 # Application definition
 
